f8f59722ff154681d6f07179e29494f7a70257ab
Rocky (Yongqiang) Gu <x_forever@126.com>
2021-04-05 17:25:23 +0800

    update sha512.c with uint64_t and corresponding constants
    Signed-off-by: Rocky (Yongqiang) Gu <x_forever@126.com>

--- sha512.c
+++ sha512.c
@@ -11,68 +11,114 @@
 #define DBG(...)
 #endif
 
-#define SHA512_BLOCK_SIZE		64	/* 512 bits = 64 Bytes */
-#define SHA512_LEN_SIZE	 	8	/* 64 bits = 8 bytes */
-#define SHA512_LEN_OFFSET 	(SHA512_BLOCK_SIZE - SHA512_LEN_SIZE)
-#define SHA512_DIGEST_SIZE	20 /* 160 bits = 20 bytes */
+#define SHA512_BLOCK_SIZE			128	/* 1024 bits = 128 bytes */
+#define SHA512_LEN_SIZE	 			 16	/*  128 bits =  16 bytes */
+#define SHA512_LEN_OFFSET 			(SHA512_BLOCK_SIZE - SHA512_LEN_SIZE)
+#define SHA512_DIGEST_SIZE			512 /* 512 bits =  64 bytes */
 
-#define SHA512_PADDING_PATTERN 	0x80
-#define SHA512_ROUND_NUM			80
+#define SHA512_PADDING_PATTERN 	   0x80
+#define SHA512_ROUND_NUM			 80
 
-typedef uint32_t (*sha512_func)(uint32_t x, uint32_t y, uint32_t z);
+#define U64(x)	x##ULL
 
 /* SHA512 Constants */
-static uint32_t K[4] = 
+static const uint64_t K512[80] = 
 {
-	0x5A827999, 0x6ED9EBA1, 0x8F1BBCDC, 0xCA62C1D6
+    U64(0x428a2f98d728ae22), U64(0x7137449123ef65cd), U64(0xb5c0fbcfec4d3b2f), U64(0xe9b5dba58189dbbc),
+    U64(0x3956c25bf348b538), U64(0x59f111f1b605d019), U64(0x923f82a4af194f9b), U64(0xab1c5ed5da6d8118),
+    U64(0xd807aa98a3030242), U64(0x12835b0145706fbe), U64(0x243185be4ee4b28c), U64(0x550c7dc3d5ffb4e2),
+    U64(0x72be5d74f27b896f), U64(0x80deb1fe3b1696b1), U64(0x9bdc06a725c71235), U64(0xc19bf174cf692694),
+    U64(0xe49b69c19ef14ad2), U64(0xefbe4786384f25e3), U64(0x0fc19dc68b8cd5b5), U64(0x240ca1cc77ac9c65),
+    U64(0x2de92c6f592b0275), U64(0x4a7484aa6ea6e483), U64(0x5cb0a9dcbd41fbd4), U64(0x76f988da831153b5),
+    U64(0x983e5152ee66dfab), U64(0xa831c66d2db43210), U64(0xb00327c898fb213f), U64(0xbf597fc7beef0ee4),
+    U64(0xc6e00bf33da88fc2), U64(0xd5a79147930aa725), U64(0x06ca6351e003826f), U64(0x142929670a0e6e70),
+    U64(0x27b70a8546d22ffc), U64(0x2e1b21385c26c926), U64(0x4d2c6dfc5ac42aed), U64(0x53380d139d95b3df),
+    U64(0x650a73548baf63de), U64(0x766a0abb3c77b2a8), U64(0x81c2c92e47edaee6), U64(0x92722c851482353b),
+    U64(0xa2bfe8a14cf10364), U64(0xa81a664bbc423001), U64(0xc24b8b70d0f89791), U64(0xc76c51a30654be30),
+    U64(0xd192e819d6ef5218), U64(0xd69906245565a910), U64(0xf40e35855771202a), U64(0x106aa07032bbd1b8),
+    U64(0x19a4c116b8d2d0c8), U64(0x1e376c085141ab53), U64(0x2748774cdf8eeb99), U64(0x34b0bcb5e19b48a8),
+    U64(0x391c0cb3c5c95a63), U64(0x4ed8aa4ae3418acb), U64(0x5b9cca4f7763e373), U64(0x682e6ff3d6b2b8a3),
+    U64(0x748f82ee5defb2fc), U64(0x78a5636f43172f60), U64(0x84c87814a1f0ab72), U64(0x8cc702081a6439ec),
+    U64(0x90befffa23631e28), U64(0xa4506cebde82bde9), U64(0xbef9a3f7b2c67915), U64(0xc67178f2e372532b),
+    U64(0xca273eceea26619c), U64(0xd186b8c721c0c207), U64(0xeada7dd6cde0eb1e), U64(0xf57d4f7fee6ed178),
+    U64(0x06f067aa72176fba), U64(0x0a637dc5a2c898a6), U64(0x113f9804bef90dae), U64(0x1b710b35131c471b),
+    U64(0x28db77f523047d84), U64(0x32caab7b40c72493), U64(0x3c9ebe0a15c9bebc), U64(0x431d67c49c100d4c),
+    U64(0x4cc5d4becb3e42b6), U64(0x597f299cfc657e2a), U64(0x5fcb6fab3ad6faec), U64(0x6c44198c4a475817)
 };
 
-/* ROTate Left (circular left shift) */
-static uint32_t ROTL(uint32_t x, uint8_t shift)
-{
-	return (x << shift) | (x >> (32 - shift));
-}
-
 #if 0
-/* ROTate Right (cirular right shift) */
-static uint32_t ROTR(uint32_t x, uint8_t shift)
+/* ROTate Left (circular left shift) */
+static uint64_t ROTL(uint64_t x, uint8_t shift)
 {
-	return (x >> shift) | (x << (32 - shift));
-}
-
-/* Right SHift */
-static uint32_t SHR(uint32_t x, uint8_t shift)
-{
-	return (x >> shift);
+	return (x << shift) | (x >> (64 - shift));
 }
 #endif
 
-/* Ch ... choose */
-static uint32_t Ch(uint32_t x, uint32_t y, uint32_t z)
+/* ROTate Right (cirular right shift) */
+static uint64_t ROTR(uint64_t x, uint8_t shift)
 {
-	DBG("    Ch(0x%08x, 0x%08x, 0x%08x);\n", x, y, z);
+	return (x >> shift) | (x << (64 - shift));
+}
+
+/* Right SHift */
+static uint64_t SHR(uint64_t x, uint8_t shift)
+{
+	return (x >> shift);
+}
+
+/* Ch ... choose */
+static uint64_t Ch(uint64_t x, uint64_t y, uint64_t z)
+{
+	DBG("    Ch(0x%016x, 0x%016x, 0x%016x);\n", x, y, z);
 	return (x & y) ^ (~x & z) ;
 }
 
-/* Par ... parity */
-static uint32_t Parity(uint32_t x, uint32_t y, uint32_t z)
-{
-	DBG("Parity(0x%08x, 0x%08x, 0x%08x);\n", x, y, z);
-	return x ^ y ^ z;
-}
-
 /* Maj ... majority */
-static uint32_t Maj(uint32_t x, uint32_t y, uint32_t z)
+static uint64_t Maj(uint64_t x, uint64_t y, uint64_t z)
 {
-	DBG("   Maj(0x%08x, 0x%08x, 0x%08x);\n", x, y, z);
+	DBG("   Maj(0x%016x, 0x%016x, 0x%016x);\n", x, y, z);
 	return (x & y) ^ (x & z) ^ (y & z);
 }
 
-/* SHA512 Functions */
-static sha512_func F[4] =
+/* Sigma0 */
+static uint64_t Sigma0(uint64_t x)
 {
-	Ch, Parity, Maj, Parity
-};
+	return ROTR(x, 28) ^ ROTR(x, 34) ^ ROTR(x, 29);
+}
+
+/* Sigma1 */
+static uint64_t Sigma1(uint64_t x)
+{
+	return ROTR(x, 14) ^ ROTR(x, 18) ^ ROTR(x, 41);
+}
+
+/* sigma0, different from Sigma0 */
+static uint64_t sigma0(uint64_t x)
+{
+	return ROTR(x, 1) ^ ROTR(x, 8) ^ SHR(x, 7);
+}
+
+/* sigma1, different from Sigma1 */
+static uint64_t sigma1(uint64_t x)
+{
+	return ROTR(x, 19) ^ ROTR(x, 61) ^ SHR(x, 6);
+}
+
+/* swap 16 bytes */
+static void *swap128(void *data)
+{
+	uint8_t *buf, temp, i;
+
+	buf = (uint8_t *)data;
+	for (i=0; i<8; i++)
+	{
+		temp = buf[i];
+		buf[i] = buf[15-i];
+		buf[15-i] = temp;
+	}
+
+	return (void *)buf;
+}
 
 static uint64_t swap64(uint64_t a)
 {
@@ -84,14 +130,6 @@ static uint64_t swap64(uint64_t a)
          ((a & 0x0000FF0000000000ULL) >> 24) |
          ((a & 0x00FF000000000000ULL) >> 40) |
          ((a & 0xFF00000000000000ULL) >> 56);
-}
-
-static uint32_t swap32(uint32_t a)
-{
-  return ((a & 0x000000FF) << 24) |
-         ((a & 0x0000FF00) << 8) |
-         ((a & 0x00FF0000) >> 8) |
-         ((a & 0xFF000000) >> 24);
 }
 
 #define DUMP_LINE_SIZE 16
@@ -122,11 +160,14 @@ int print_buffer(void *buf, uint32_t len)
  */
 
 struct sha512_context {
-	uint32_t a;
-	uint32_t b;
-	uint32_t c;
-	uint32_t d;
-	uint32_t e;
+	uint64_t a;
+	uint64_t b;
+	uint64_t c;
+	uint64_t d;
+	uint64_t e;
+	uint64_t f;
+	uint64_t g;
+	uint64_t h;
 
 	uint32_t processed_blocks;
 
@@ -149,11 +190,14 @@ int sha512_init(void)
 	context = get_sha512_context();
 
 	memset(context, 0, sizeof(struct sha512_context));
-	context->a = 0x67452301;
-	context->b = 0xEFCDAB89;
-	context->c = 0x98BADCFE;
-	context->d = 0x10325476;
-	context->e = 0xC3D2E1F0;
+	context->a = U64(0x6a09e667f3bcc908);
+	context->b = U64(0xbb67ae8584caa73b);
+	context->c = U64(0x3c6ef372fe94f82b);
+	context->d = U64(0xa54ff53a5f1d36f1);
+	context->e = U64(0x510e527fade682d1);
+	context->c = U64(0x9b05688c2b3e6c1f);
+	context->d = U64(0x1f83d9abfb41bd6b);
+	context->e = U64(0x5be0cd19137e2179);
 
 	context->processed_blocks = 0;
 	context->rest = 0;
@@ -161,16 +205,16 @@ int sha512_init(void)
 	return 0;
 }
 
-#define WORD(b,i) (((uint32_t *)b)[i])
-static uint32_t prepare_schedule_word(const void *block, uint32_t *w)
+#define WORD(b,i) (((uint64_t *)b)[i])
+static uint32_t prepare_schedule_word(const void *block, uint64_t *w)
 {
-	uint32_t i;
-	for (i=0; i<SHA512_ROUND_NUM; i++)
+	uint32_t t;
+	for (t=0; t<SHA512_ROUND_NUM; t++)
 	{
-		if (i<=15)
-			w[i] = swap32(WORD(block, i));
-		else
-			w[i] = ROTL(w[i-3] ^ w[i-8] ^ w[i-14] ^ w[i-16], 1);
+		if (t<=15) /*  0 <= t <= 15 */
+			w[t] = swap64(WORD(block, t));
+		else	   /* 16 <= t <= 79 */
+			w[t] = sigma1(w[t-2] + w[t-7] + sigma0(w[t-15]) + w[t-16];
 	}
 
 	return 0;
@@ -178,9 +222,9 @@ static uint32_t prepare_schedule_word(const void *block, uint32_t *w)
 
 static uint32_t sha512_process_block(const void *block)
 {
-	uint32_t W[SHA512_ROUND_NUM];
-	uint32_t t, T;
-	uint32_t a, b, c, d, e;
+	uint64_t W[SHA512_ROUND_NUM];
+	uint64_t t, T1, T2;
+	uint64_t a, b, c, d, e, f, g, h;
 	struct sha512_context *context;
 
 	context = get_sha512_context();
@@ -198,15 +242,22 @@ static uint32_t sha512_process_block(const void *block)
 	c = context->c;
 	d = context->d;
 	e = context->e;
+	f = context->f;
+	g = context->g;
+	h = context->h;
 
 	for (t=0; t<SHA512_ROUND_NUM; t++)
 	{
-		T = ROTL(a, 5) + (F[t/20])(b, c, d) + e + K[t/20] + W[t];
-		e = d;
-		d = c;
-		c = ROTL(b, 30);
-		b = a;
-		a = T;
+		T1 = h + Sigma1(e) + Ch(e, f, g) + K512[t] + W[t];
+		T2 = Sigma0(a) + Maj(a, b, c);
+		 h = g;
+		 g = f;
+		 f = e;
+		 e = d + T1;
+		 d = c;
+		 c = b;
+		 b = a;
+		 a = T1 + T2;
 
 #if 0 //def DEBUG
 		DBG("%02d:\n", t);
@@ -220,6 +271,9 @@ static uint32_t sha512_process_block(const void *block)
 	context->c += c;
 	context->d += d;
 	context->e += e;
+	context->f += f;
+	context->g += g;
+	context->h += h;
 
 	context->processed_blocks++;
 
@@ -322,7 +376,8 @@ int sha512_final(uint8_t *hash)
 		context->rest = 0;
 
 		memset(&context->last_block[0], 0, SHA512_BLOCK_SIZE - SHA512_LEN_SIZE);
-		*(uint64_t *)&(context->last_block[SHA512_LEN_OFFSET]) = swap64(total);
+		//*(uint64_t *)&(context->last_block[SHA512_LEN_OFFSET]) = swap64(total);
+		swap128(&context->last_block[SHA512_LEN_OFFSET]);
 		sha512_process_block(&context->last_block);
 	}
 	else /* 0 <= rest < SHA512_BLOCK_SIZE - SHA512_LEN_SIZE */
@@ -341,14 +396,18 @@ int sha512_final(uint8_t *hash)
 		sha512_process_block(&context->last_block);
 	}
 
-	DBG("%08x %08x %08x %08x %08x\n", context->a, context->b, context->c, context->d, context->e);
-	//snprintf(hash, SHA512_DIGEST_SIZE, "%08x%08x%08x%08x%08x", context->a, context->b, context->c, context->d, context->e);
-	buf = (uint32_t *)hash;
-	buf[0] = swap32(context->a);
-	buf[1] = swap32(context->b);
-	buf[2] = swap32(context->c);
-	buf[3] = swap32(context->d);
-	buf[4] = swap32(context->e);
+	DBG("%016x %016x %016x %016x %016x\n", 
+		context->a, context->b, context->c, context->d, context->e, context->f, context->g, context->g);
+
+	buf = (uint64_t *)hash;
+	buf[0] = swap64(context->a);
+	buf[1] = swap64(context->b);
+	buf[2] = swap64(context->c);
+	buf[3] = swap64(context->d);
+	buf[4] = swap64(context->e);
+	buf[5] = swap64(context->f);
+	buf[6] = swap64(context->g);
+	buf[7] = swap64(context->h);
 
 	return 0;
 }
