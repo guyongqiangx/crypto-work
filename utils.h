dae3cbe921b68ae23d4c0de282ae5a06790e6a41
Rocky (Yongqiang) Gu <x_forever@126.com>
2021-05-30 08:09:20 +0800

    修复加入stblib.h后的重复定义的错误
    Signed-off-by: Rocky (Yongqiang) Gu <x_forever@126.com>

--- utils.h
+++ utils.h
@@ -1,4 +1,5 @@
 #ifndef __UTILS__H
+#include <stdlib.h>
 
 /*
  * from: linux/arch/arm/kernel/setup.c
@@ -13,19 +14,24 @@ static union { char c[4]; unsigned long l; } endian_test = { { 'l', '?', '?', 'b
  * from: /usr/include/x86_64-linux-gnu/bits/byteswap.h
  */
 /* Swap bytes in 16 bit value.  */
+#ifndef __bswap_constant_16
 #define __bswap_constant_16(x) \
      ((unsigned short int)     \
 	  ((((x) >> 8) & 0xff)     \
 	 | (((x) & 0xff) << 8)))
+#endif
 
 /* Swap bytes in 32 bit value.  */
+#ifndef __bswap_constant_32
 #define __bswap_constant_32(x)     \
      ((((x) & 0xff000000) >> 24)   \
 	 | (((x) & 0x00ff0000) >>  8) \
 	 | (((x) & 0x0000ff00) <<  8) \
 	 | (((x) & 0x000000ff) << 24))
+#endif
 
 /* Swap bytes in 64 bit value.  */
+#ifndef __bswap_constant_64
 #define __bswap_constant_64(x)                \
      ((((x) & 0xff00000000000000ull) >> 56)  \
      | (((x) & 0x00ff000000000000ull) >> 40) \
@@ -35,18 +41,25 @@ static union { char c[4]; unsigned long l; } endian_test = { { 'l', '?', '?', 'b
      | (((x) & 0x0000000000ff0000ull) << 24) \
      | (((x) & 0x000000000000ff00ull) << 40) \
      | (((x) & 0x00000000000000ffull) << 56))
+#endif
 
 /*
  * host to big endian
  */
+#ifndef htobe16
 #define htobe16(x) \
 	((ENDIANNESS == ENDIAN_LITTLE) ? __bswap_constant_16(x) : (x))
+#endif
 
+#ifndef htobe32
 #define htobe32(x) \
 	((ENDIANNESS == ENDIAN_LITTLE) ? __bswap_constant_32(x) : (x))
+#endif
 
+#ifndef htobe64
 #define htobe64(x) \
 	((ENDIANNESS == ENDIAN_LITTLE) ? __bswap_constant_64(x) : (x))
+#endif
 
 /*
  * unsigned short int htobe16(unsigned short int x)
