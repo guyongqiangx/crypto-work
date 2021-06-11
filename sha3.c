e89b867c071c721eac7e3ce687b7377472f1a347
Rocky Gu <rocky.gu@broadcom.com>
2021-06-11 10:25:24 +0800

    删除从SHA512复制过来但SHA3没有用的代码
    Signed-off-by: Rocky Gu <rocky.gu@broadcom.com>

--- sha3.c
+++ sha3.c
@@ -20,54 +20,6 @@
 #define DUMP_ROUND_DATA 0
 #endif
 
-#if 0
-#define SHA3_BLOCK_SIZE			128	/* 1024 bits = 128 bytes */
-#define SHA3_LEN_SIZE	 			 16	/*  128 bits =  16 bytes */
-#define SHA3_LEN_OFFSET 			(SHA3_BLOCK_SIZE - SHA3_LEN_SIZE)
-#define SHA3_DIGEST_SIZE			 64 /*  512 bits =  64 bytes */
-
-#define SHA3_PADDING_PATTERN 	   0x80
-#define SHA3_ROUND_NUM			 80
-
-#define SHA384_DIGEST_SIZE			 48 /*  384 bits =  48 bytes */
-#define SHA3_224_DIGEST_SIZE	     28 /*  224 bits =  28 bytes */
-#define SHA3_256_DIGEST_SIZE	     32 /*  256 bits =  32 bytes */
-
-#define HASH_BLOCK_SIZE		SHA3_BLOCK_SIZE
-#define HASH_LEN_SIZE		SHA3_LEN_SIZE
-#define HASH_LEN_OFFSET		SHA3_LEN_OFFSET
-
-#define HASH_DIGEST_SIZE	SHA3_DIGEST_SIZE
-
-#define HASH_PADDING_PATTERN	SHA3_PADDING_PATTERN
-#define HASH_ROUND_NUM			SHA3_ROUND_NUM
-
-/* SHA3 Constants */
-static const uint64_t K512[HASH_ROUND_NUM] =
-{
-    0x428a2f98d728ae22, 0x7137449123ef65cd, 0xb5c0fbcfec4d3b2f, 0xe9b5dba58189dbbc,
-    0x3956c25bf348b538, 0x59f111f1b605d019, 0x923f82a4af194f9b, 0xab1c5ed5da6d8118,
-    0xd807aa98a3030242, 0x12835b0145706fbe, 0x243185be4ee4b28c, 0x550c7dc3d5ffb4e2,
-    0x72be5d74f27b896f, 0x80deb1fe3b1696b1, 0x9bdc06a725c71235, 0xc19bf174cf692694,
-    0xe49b69c19ef14ad2, 0xefbe4786384f25e3, 0x0fc19dc68b8cd5b5, 0x240ca1cc77ac9c65,
-    0x2de92c6f592b0275, 0x4a7484aa6ea6e483, 0x5cb0a9dcbd41fbd4, 0x76f988da831153b5,
-    0x983e5152ee66dfab, 0xa831c66d2db43210, 0xb00327c898fb213f, 0xbf597fc7beef0ee4,
-    0xc6e00bf33da88fc2, 0xd5a79147930aa725, 0x06ca6351e003826f, 0x142929670a0e6e70,
-    0x27b70a8546d22ffc, 0x2e1b21385c26c926, 0x4d2c6dfc5ac42aed, 0x53380d139d95b3df,
-    0x650a73548baf63de, 0x766a0abb3c77b2a8, 0x81c2c92e47edaee6, 0x92722c851482353b,
-    0xa2bfe8a14cf10364, 0xa81a664bbc423001, 0xc24b8b70d0f89791, 0xc76c51a30654be30,
-    0xd192e819d6ef5218, 0xd69906245565a910, 0xf40e35855771202a, 0x106aa07032bbd1b8,
-    0x19a4c116b8d2d0c8, 0x1e376c085141ab53, 0x2748774cdf8eeb99, 0x34b0bcb5e19b48a8,
-    0x391c0cb3c5c95a63, 0x4ed8aa4ae3418acb, 0x5b9cca4f7763e373, 0x682e6ff3d6b2b8a3,
-    0x748f82ee5defb2fc, 0x78a5636f43172f60, 0x84c87814a1f0ab72, 0x8cc702081a6439ec,
-    0x90befffa23631e28, 0xa4506cebde82bde9, 0xbef9a3f7b2c67915, 0xc67178f2e372532b,
-    0xca273eceea26619c, 0xd186b8c721c0c207, 0xeada7dd6cde0eb1e, 0xf57d4f7fee6ed178,
-    0x06f067aa72176fba, 0x0a637dc5a2c898a6, 0x113f9804bef90dae, 0x1b710b35131c471b,
-    0x28db77f523047d84, 0x32caab7b40c72493, 0x3c9ebe0a15c9bebc, 0x431d67c49c100d4c,
-    0x4cc5d4becb3e42b6, 0x597f299cfc657e2a, 0x5fcb6fab3ad6faec, 0x6c44198c4a475817
-};
-#else
-
 /*
  * |---------------|------------------------|
  * | Padding Bytes | Padding Message        |
@@ -86,67 +38,12 @@ static const uint64_t K512[HASH_ROUND_NUM] =
 /* b'01 100000...00000001, 0x60...0x01 */
 #define SHA3_PADDING_PAT2_BEGIN  0x06
 #define SHA3_PADDING_PAT2_END    0x80
-#endif
 
-#if 1
 /* ROTate Left (circular left shift) */
 static uint64_t ROTL(uint64_t x, uint8_t shift)
 {
 	return (x << shift) | (x >> (64 - shift));
 }
-#endif
-
-/* ROTate Right (cirular right shift) */
-static uint64_t ROTR(uint64_t x, uint8_t shift)
-{
-	return (x >> shift) | (x << (64 - shift));
-}
-
-#if 0
-/* Right SHift */
-static uint64_t SHR(uint64_t x, uint8_t shift)
-{
-	return (x >> shift);
-}
-
-/* Ch ... choose */
-static uint64_t Ch(uint64_t x, uint64_t y, uint64_t z)
-{
-	//DBG("    Ch(0x%016llx, 0x%016llx, 0x%016llx);\n", x, y, z);
-	return (x & y) ^ (~x & z) ;
-}
-
-/* Maj ... majority */
-static uint64_t Maj(uint64_t x, uint64_t y, uint64_t z)
-{
-	//DBG("   Maj(0x%016llx, 0x%016llx, 0x%016llx);\n", x, y, z);
-	return (x & y) ^ (x & z) ^ (y & z);
-}
-
-/* SIGMA0 */
-static uint64_t SIGMA0(uint64_t x)
-{
-	return ROTR(x, 28) ^ ROTR(x, 34) ^ ROTR(x, 39);
-}
-
-/* SIGMA1 */
-static uint64_t SIGMA1(uint64_t x)
-{
-	return ROTR(x, 14) ^ ROTR(x, 18) ^ ROTR(x, 41);
-}
-
-/* sigma0, different from SIGMA0 */
-static uint64_t sigma0(uint64_t x)
-{
-	return ROTR(x, 1) ^ ROTR(x, 8) ^ SHR(x, 7);
-}
-
-/* sigma1, different from SIGMA1 */
-static uint64_t sigma1(uint64_t x)
-{
-	return ROTR(x, 19) ^ ROTR(x, 61) ^ SHR(x, 6);
-}
-#endif
 
 static uint32_t theta(uint64_t A[5][5])
 {
