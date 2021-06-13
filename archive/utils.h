3a9b5c06c359637fdaa89f3349cc7db2c46af88e
Rocky (Yongqiang) Gu <x_forever@126.com>
2021-06-13 23:37:14 +0800

    同步md4代码目录下的utils.h, utils.c文件
    Signed-off-by: Rocky (Yongqiang) Gu <x_forever@126.com>

--- archive/utils.h
+++ archive/utils.h
@@ -24,17 +24,17 @@ static union { char c[4]; unsigned long l; } endian_test = { { 'l', '?', '?', 'b
 #ifndef __bswap_constant_16
 #define __bswap_constant_16(x) \
      ((unsigned short int)     \
-	  ((((x) >> 8) & 0xff)     \
-	 | (((x) & 0xff) << 8)))
+      ((((x) >> 8) & 0xff)     \
+     | (((x) & 0xff) << 8)))
 #endif
 
 /* Swap bytes in 32 bit value.  */
 #ifndef __bswap_constant_32
 #define __bswap_constant_32(x)     \
      ((((x) & 0xff000000) >> 24)   \
-	 | (((x) & 0x00ff0000) >>  8) \
-	 | (((x) & 0x0000ff00) <<  8) \
-	 | (((x) & 0x000000ff) << 24))
+     | (((x) & 0x00ff0000) >>  8) \
+     | (((x) & 0x0000ff00) <<  8) \
+     | (((x) & 0x000000ff) << 24))
 #endif
 
 /* Swap bytes in 64 bit value.  */
@@ -55,17 +55,17 @@ static union { char c[4]; unsigned long l; } endian_test = { { 'l', '?', '?', 'b
  */
 #ifndef htobe16
 #define htobe16(x) \
-	((ENDIANNESS == ENDIAN_LITTLE) ? __bswap_constant_16(x) : (x))
+    ((ENDIANNESS == ENDIAN_LITTLE) ? __bswap_constant_16(x) : (x))
 #endif
 
 #ifndef htobe32
 #define htobe32(x) \
-	((ENDIANNESS == ENDIAN_LITTLE) ? __bswap_constant_32(x) : (x))
+    ((ENDIANNESS == ENDIAN_LITTLE) ? __bswap_constant_32(x) : (x))
 #endif
 
 #ifndef htobe64
 #define htobe64(x) \
-	((ENDIANNESS == ENDIAN_LITTLE) ? __bswap_constant_64(x) : (x))
+    ((ENDIANNESS == ENDIAN_LITTLE) ? __bswap_constant_64(x) : (x))
 #endif
 
 /*
@@ -73,17 +73,17 @@ static union { char c[4]; unsigned long l; } endian_test = { { 'l', '?', '?', 'b
  */
 #ifndef be16toh
 #define be16toh(x) \
-	((ENDIANNESS == ENDIAN_LITTLE) ? __bswap_constant_16(x) : (x))
+    ((ENDIANNESS == ENDIAN_LITTLE) ? __bswap_constant_16(x) : (x))
 #endif
 
 #ifndef be32toh
 #define be32toh(x) \
-	((ENDIANNESS == ENDIAN_LITTLE) ? __bswap_constant_32(x) : (x))
+    ((ENDIANNESS == ENDIAN_LITTLE) ? __bswap_constant_32(x) : (x))
 #endif
 
 #ifndef be64toh
 #define be64toh(x) \
-	((ENDIANNESS == ENDIAN_LITTLE) ? __bswap_constant_64(x) : (x))
+    ((ENDIANNESS == ENDIAN_LITTLE) ? __bswap_constant_64(x) : (x))
 #endif
 
 int htole32c(unsigned char *data, unsigned long x);
