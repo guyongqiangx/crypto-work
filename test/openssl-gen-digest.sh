556ceb7ed4a4aa37c56c55d45fe6d74c0d6f4b25
Rocky (Yongqiang) Gu <x_forever@126.com>
2021-06-13 23:44:15 +0800

    更新测试test目录
    Signed-off-by: Rocky (Yongqiang) Gu <x_forever@126.com>

---
+++ test/openssl-gen-digest.sh
@@ -0,0 +1,15 @@
+# echo -n "" | openssl dgst -sha512
+# echo -n "a" | openssl dgst -sha512
+# echo -n "abc" | openssl dgst -sha512
+# echo -n "message digest" | openssl dgst -sha512
+# echo -n "abcdefghijklmnopqrstuvwxyz" | openssl dgst -sha512
+# echo -n "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789" | openssl dgst -sha512
+# echo -n "12345678901234567890123456789012345678901234567890123456789012345678901234567890" | openssl dgst -sha512
+# 
+# echo -n "" | openssl dgst -sm3
+# echo -n "a" | openssl dgst -sm3
+# echo -n "abc" | openssl dgst -sm3
+# echo -n "message digest" | openssl dgst -sm3
+# echo -n "abcdefghijklmnopqrstuvwxyz" | openssl dgst -sm3
+# echo -n "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789" | openssl dgst -sm3
+# echo -n "12345678901234567890123456789012345678901234567890123456789012345678901234567890" | openssl dgst -sm3
