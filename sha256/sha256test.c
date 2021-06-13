41c35fd539ffc9c75c617e306c05a80edcaac804
Rocky (Yongqiang) Gu <x_forever@126.com>
2021-06-13 21:23:54 +0800

    SHA256: 独立的sha256源码，使用新的sha256test.c
    Signed-off-by: Rocky (Yongqiang) Gu <x_forever@126.com>

---
+++ sha256/sha256test.c
@@ -0,0 +1,263 @@
+#include <stdio.h>  /* printf, fopen, fread, fclose... */
+#include <stdlib.h> /* exit */
+#include <string.h> /* strlen */
+#include <unistd.h> /* getopt */
+
+#include "sha256.h"
+
+#define HASH_DIGEST_SIZE    32      /* sha256 digest size */
+#define FILE_BLOCK_SIZE     1024
+
+/*
+ * Print a usage message
+ */
+void usage(const char *argv0)
+{
+    fprintf(stderr,
+        "Usage:\n"
+        "Common options: [-x|-f file|-s string|-h]\n"
+        "Hash a string:\n"
+            "\t%s -s string\n"
+        "Hash a file:\n"
+            "\t%s -f file [-k key]\n"
+        "-x\tInternal string hash test\n"
+        "-h\tDisplay this message\n"
+        , argv0, argv0);
+    exit(1);
+}
+
+/*
+ * Print a message digest in hexadecimal
+ */
+static int print_digest(unsigned char *digest)
+{
+    uint32_t i;
+
+    for (i = 0; i < HASH_DIGEST_SIZE; i++)
+    {
+        printf ("%02x", digest[i]);
+    }
+
+    return 0;
+}
+
+struct HASH_ITEM {
+    char        *str;
+    uint32_t    len;
+    unsigned char md[HASH_DIGEST_SIZE*2];
+} hashes[] =
+{
+    { /* 0 */
+        "",
+        0,
+        "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    },
+    { /* 1 */
+        "a",
+        1,
+        "ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb"
+    },
+    { /* 2 */
+        "abc",
+        3,
+        "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"
+    },
+    { /* 3 */
+        "message digest",
+        14,
+        "f7846f55cf23e14eebeab5b4e1550cad5b509e3348fbc4efa3a1413d393cb650"
+    },
+    { /* 4 */
+        "abcdefghijklmnopqrstuvwxyz",
+        26,
+        "71c480df93d6ae2f1efad1447c66c9525e316218cf51fc8d9ed832f2daf18b73"
+    },
+    { /* 5 */
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+        62,
+        "db4bfcbd4da0cd85a60c3c37d3fbd8805c77f15fc6b1fdfe614ee0a7c8fdb4c0"
+    },
+    { /* 6 */
+        "12345678901234567890123456789012345678901234567890123456789012345678901234567890",
+        80,
+        "f371bc4a311f2b009eef952dd83ca80e2b60026c8e935592d0f9c308453c813e"
+    },
+};
+
+/*
+ * Internal digest tests
+ */
+static int internal_digest_tests(const char *argv0)
+{
+    unsigned char digest[HASH_DIGEST_SIZE];
+    struct HASH_ITEM *item;
+
+    printf ("Internal hash tests for %s:\n", argv0);
+
+    for (item=&hashes[0]; item<(&hashes[0]+sizeof(hashes)/sizeof(hashes[0])); item++)
+    {
+        SHA256((unsigned char*)item->str, item->len, digest);
+        printf("%s(\"%s\")\n", argv0, item->str);
+        printf("Expect: %s\n", item->md);
+        printf("Result: ");
+        print_digest(digest);
+        printf("\n\n");
+    }
+
+    return 0;
+}
+
+/*
+ * Hash a string and print the digest
+ */
+static int digest_string(const char *argv0, const unsigned char *string, uint32_t len)
+{
+    unsigned char digest[HASH_DIGEST_SIZE];
+
+    SHA256(string, len, digest);
+
+    printf("%s(\"%s\") = ", argv0, string);
+    print_digest(digest);
+    printf("\n");
+
+    return 0;
+}
+
+/*
+ * Hash a file and print the digest
+ */
+static int digest_file(const char *argv0, const char *filename)
+{
+    SHA256_CTX c;
+    FILE *f;
+
+    unsigned char digest[HASH_DIGEST_SIZE];
+    unsigned char buf[FILE_BLOCK_SIZE];
+
+    int len = 0;
+    int rc = 0;
+
+    f = fopen(filename, "rb");
+    if (NULL == f)
+    {
+        printf("Can't open file %s\n", filename);
+        rc = -1;
+    }
+    else
+    {
+        SHA256_Init(&c);
+        while ((len = fread(buf, 1, FILE_BLOCK_SIZE, f)))
+        {
+            SHA256_Update(&c, buf, len);
+        }
+        SHA256_Final(digest, &c);
+
+        fclose(f);
+
+        printf("%s(%s) = ", argv0, filename);
+        print_digest(digest);
+        printf("\n");
+
+        rc = 0;
+    }
+
+    return rc;
+}
+
+/*
+ * Hash the standard input and prints the digest
+ */
+static void digest_stdin(const char *argv0)
+{
+    SHA256_CTX c;
+
+    int len;
+    unsigned char digest[HASH_DIGEST_SIZE];
+    unsigned char buf[HASH_DIGEST_SIZE];
+
+    SHA256_Init(&c);
+    while ((len = fread(buf, 1, HASH_DIGEST_SIZE, stdin)))
+    {
+        SHA256_Update(&c, buf, len);
+    }
+    SHA256_Final(digest, &c);
+
+    printf("%s(stdin) = ", argv0);
+    print_digest(digest);
+    printf("\n");
+}
+
+/*
+ * $ sha256 -h
+ * Usage:
+ * Common options: [-x|-f file|-s string|-h]
+ * Hash a string:
+ *         sha256 -s string
+ * Hash a file:
+ *         sha256 -f file [-k key]
+ * -x      Internal string hash test
+ * -h      Display this message
+ */
+int main(int argc, char *argv[])
+{
+    int ch;
+    int hash_internal = 0;
+    int hash_str = 0;
+    int hash_file = 0;
+    int hash_stdin = 0;
+
+    char *str = NULL;
+    uint32_t len = 0;
+
+    char *filename = NULL;
+
+    while ((ch = getopt(argc, argv, "s:f:xh")) != -1)
+    {
+        switch(ch)
+        {
+            case 'x':
+                hash_internal = 1;
+                break;
+            case 's':
+                hash_str = 1;
+                str = optarg;
+                len = strlen(str);
+                break;
+            case 'f':
+                hash_file = 1;
+                filename = optarg;
+                break;
+            case 'h':
+            default: /* '?' */
+                usage(argv[0]);
+                break;
+        }
+    }
+
+    if (argc == 1)
+    {
+        hash_stdin = 1;
+    }
+
+    if (hash_internal)
+    {
+        internal_digest_tests(argv[0]);
+    }
+
+    if (hash_str)
+    {
+        digest_string(argv[0], (unsigned char *)str, len);
+    }
+
+    if (hash_file)
+    {
+        digest_file(argv[0], filename);
+    }
+
+    if (hash_stdin)
+    {
+        digest_stdin(argv[0]);
+    }
+
+    return 0;
+}
