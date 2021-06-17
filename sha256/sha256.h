4e770c22084032fd9e21c0b15b83ecec04cc36cf
Rocky Gu <rocky.gu@broadcom.com>
2021-06-17 19:36:04 +0800

    更新头文件中的tab，转为space
    Signed-off-by: Rocky Gu <rocky.gu@broadcom.com>

--- sha256/sha256.h
+++ sha256/sha256.h
@@ -1,10 +1,10 @@
 #ifndef __ROCKY_SHA256__H
 #define __ROCKY_SHA256__H
 
-#define ERR_OK			 0
-#define ERR_ERR         -1	/* generic error */
-#define ERR_INV_PARAM	-2  /* invalid parameter */
-#define ERR_TOO_LONG	-3  /* too long */
+#define ERR_OK           0
+#define ERR_ERR         -1  /* generic error */
+#define ERR_INV_PARAM   -2  /* invalid parameter */
+#define ERR_TOO_LONG    -3  /* too long */
 #define ERR_STATE_ERR   -4  /* state error */
 
 typedef unsigned char      uint8_t;
--- sha3/sha3.h
+++ sha3/sha3.h
@@ -2,15 +2,15 @@
 #define __ROCKY_SHA3__H
 
 #define ERR_OK           0
-#define ERR_ERR         -1	/* generic error */
+#define ERR_ERR         -1  /* generic error */
 #define ERR_INV_PARAM   -2  /* invalid parameter */
 #define ERR_TOO_LONG    -3  /* too long */
 #define ERR_STATE_ERR   -4  /* state error */
 
-typedef unsigned char       uint8_t;
-typedef unsigned short      uint16_t;
-typedef unsigned int        uint32_t;
-typedef unsigned long long  uint64_t;
+typedef unsigned char      uint8_t;
+typedef unsigned short     uint16_t;
+typedef unsigned int       uint32_t;
+typedef unsigned long long uint64_t;
 typedef struct {
     uint64_t high; /* high 64 bits */
     uint64_t low;  /*  low 64 bits */
