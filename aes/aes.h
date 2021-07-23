0fa6e2683f0693eb73211b83f1175fb525f43357
Yongqiang Gu <x_forever@126.com>
2021-07-23 15:20:34 +0800

    添加 AES 的文档和代码文件
    Signed-off-by: Yongqiang Gu <x_forever@126.com>

---
+++ aes/aes.h
@@ -0,0 +1,24 @@
+/*
+ * @        file: des.h
+ * @ description: header file for aes.c
+ * @      author: Gu Yongqiang
+ * @        blog: https://blog.csdn.net/guyongqiangx
+ */
+#ifndef __ROCKY_DES__H
+#define __ROCKY_DES__H
+
+#define ERR_OK           0
+#define ERR_ERR         -1  /* generic error */
+#define ERR_INV_PARAM   -2  /* invalid parameter */
+#define ERR_TOO_LONG    -3  /* too long */
+#define ERR_STATE_ERR   -4  /* state error */
+
+typedef unsigned char      uint8_t;
+typedef unsigned short     uint16_t;
+typedef unsigned int       uint32_t;
+typedef unsigned long long uint64_t;
+
+int AES_Encryption(const unsigned char *data, unsigned int data_len, const unsigned char *key, unsigned int key_len, const unsigned *out, unsigned int out_len);
+int AES_Decryption(const unsigned char *data, unsigned int data_len, const unsigned char *key, unsigned int key_len, const unsigned *out, unsigned int out_len);
+
+#endif
