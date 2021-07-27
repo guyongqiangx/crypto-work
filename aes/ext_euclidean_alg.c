7d511005ea2e1fe061e5781801014b4220c580fa
Yongqiang Gu <x_forever@126.com>
2021-07-27 16:19:55 +0800

    AES: 添加polynomial用于计算GF(2^n)域内的多项式乘法
    Signed-off-by: Yongqiang Gu <x_forever@126.com>

--- aes/ext_euclidean_alg.c
+++ aes/ext_euclidean_alg.c
@@ -5,6 +5,7 @@
  * @        blog: https://blog.csdn.net/guyongqiangx
  */
 #include <stdio.h>
+#include <stdint.h>
 
 int gcd(int a, int b)
 {
@@ -48,12 +49,48 @@ int inverse(int a, int b)
     return y;
 }
 
+/* a x b mod m(x) */
+uint8_t polonomial_xtime(uint8_t a, uint8_t b)
+{
+    uint8_t m, res;
+
+    m = a;
+    res = 0;
+    while (b != 0)
+    {
+        if (b & 0x01)
+        {
+            if (0 == res)
+            {
+                res = m;
+            }
+            else
+            {
+                res ^= m;
+            }
+        }
+        if (m & 0x80)   /* b7=1 */
+        {
+            m = (m << 1) ^ 0x1B; /* 0x1B = b00011011, m(x) = x4 + x3 + x + 1 */
+        }
+        else            /* b7=0 */
+        {
+            m <<= 1;
+        }
+        b >>= 1;
+    }
+
+    return res;
+}
+
 int main(int argc, char *argv)
 {
     printf("gcd(%d, %d) = %d\n", 52, 76, gcd(52, 76));
     printf("gcd(%d, %d) = %d\n", 243, 77, gcd(243, 77));
     printf("invers(%d, %d) = %d\n", 243, 77, inverse(243, 77));
     printf("invers(%d, %d) = %d\n", 1759, 550, inverse(1759, 550));
+
+    printf("xtime(0x%02x, 0x%02x) = 0x%02x\n", 0x57, 0x83, polonomial_xtime(0x57, 0x83));
 
     return 0;
 }
