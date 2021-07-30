8adda7650b36b0e18ac3050c20c788f82abb3d1c
Yongqiang Gu <x_forever@126.com>
2021-07-30 19:04:04 +0800

    AES: 更新常量数组定义, 新增AES算法枚举
    Signed-off-by: Yongqiang Gu <x_forever@126.com>

--- aes/aes.h
+++ aes/aes.h
@@ -18,6 +18,12 @@ typedef unsigned short     uint16_t;
 typedef unsigned int       uint32_t;
 typedef unsigned long long uint64_t;
 
+typedef enum aes_algorithm {
+    AES128,
+    AES192,
+    AES256,
+}AES_ALG;
+
 int AES_Encryption(const unsigned char *data, unsigned int data_len, const unsigned char *key, unsigned int key_len, const unsigned *out, unsigned int out_len);
 int AES_Decryption(const unsigned char *data, unsigned int data_len, const unsigned char *key, unsigned int key_len, const unsigned *out, unsigned int out_len);
 
