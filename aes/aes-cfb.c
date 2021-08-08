c5dbdb07480204d3a173719d4db5119eb0594063
Rocky (Yongqiang) Gu <x_forever@126.com>
2021-08-09 01:06:45 +0800

    AES: 添加AES其他模式的文件
    Signed-off-by: Rocky (Yongqiang) Gu <x_forever@126.com>

---
+++ aes/aes-cfb.c
@@ -0,0 +1,14 @@
+/*
+ * @        file: aes-cfb.c
+ * @ description: implementation for the AES Cipher FeedBack (CFB) Mode
+ * @      author: Gu Yongqiang
+ * @        blog: https://blog.csdn.net/guyongqiangx
+ */
+#include <stdio.h>
+#include <string.h>
+
+#include "utils.h"
+#include "aes.h"
+
+#define AES_BLOCK_SIZE      16
+
---
+++ aes/aes-ctr.c
@@ -0,0 +1,105 @@
+/*
+ * @        file: 
+ * @ description: 
+ * @      author: Gu Yongqiang
+ * @        blog: https://blog.csdn.net/guyongqiangx
+ */
+/*
+ * @        file: aes-ctr.c
+ * @ description: implementation for the AES Counter (CTR) Mode
+ * @      author: Gu Yongqiang
+ * @        blog: https://blog.csdn.net/guyongqiangx
+ */
+#include <stdio.h>
+#include <string.h>
+
+#include "utils.h"
+#include "aes.h"
+
+#define AES_BLOCK_SIZE      16
+
+static void xor(const uint8_t *iv, const uint8_t *in, uint8_t *out)
+{
+    int i;
+    for (i=0; i<AES_BLOCK_SIZE; i++)
+    {
+        *out ++ = (*iv ++) ^ (*in ++);
+    }
+}
+
+int AES_CTR_Encrypt(AES_ALG alg, const unsigned char *in, unsigned int size, const unsigned char *key, const unsigned char *nonce, unsigned char *out)
+{
+//    unsigned char vector[AES_BLOCK_SIZE], temp[AES_BLOCK_SIZE];
+//    int i;
+//
+//    if ((NULL == in) || (NULL == key) || (NULL == iv) || (NULL == out) || (0 == size))
+//    {
+//        return ERR_INV_PARAM;
+//    }
+//
+//    memcpy(vector, iv, AES_BLOCK_SIZE);
+//
+//    while (size >= AES_BLOCK_SIZE)
+//    {
+//        xor(vector, in, temp);
+//
+//        AES_Encrypt(alg, temp, key, out);
+//
+//        memcpy(vector, out, AES_BLOCK_SIZE);
+//        in  += AES_BLOCK_SIZE;
+//        out += AES_BLOCK_SIZE;
+//
+//        size -= AES_BLOCK_SIZE;
+//    }
+//
+//    if (size > 0)
+//    {
+//        memcpy(temp, in, size);     /* 0 ~ size-1 */
+//        temp[size] = 0x80;          /* size */
+//        memset(&temp[size+1], 0, AES_BLOCK_SIZE-size-1); /* size ~ AES_BLOCK_SIZE-1 */
+//
+//        xor(vector, temp, temp);
+//
+//        AES_Encrypt(alg, temp, key, out);
+//    }
+
+    return ERR_OK;
+}
+
+int AES_CTR_Decrypt(AES_ALG alg, const unsigned char *in, unsigned int size, const unsigned char *key, const unsigned char *nonce, unsigned char *out)
+{
+//    unsigned char vector[AES_BLOCK_SIZE], temp[AES_BLOCK_SIZE];
+//    int i;
+//
+//    if ((NULL == in) || (NULL == key) || (NULL == iv) || (NULL == out) || (0 == size))
+//    {
+//        return ERR_INV_PARAM;
+//    }
+//
+//    memcpy(vector, iv, AES_BLOCK_SIZE);
+//
+//    while (size >= AES_BLOCK_SIZE)
+//    {
+//        AES_Decrypt(alg, in, key, temp);
+//        
+//        xor(vector, temp, out);
+//
+//        memcpy(vector, in, AES_BLOCK_SIZE);
+//        in  += AES_BLOCK_SIZE;
+//        out += AES_BLOCK_SIZE;
+//
+//        size -= AES_BLOCK_SIZE;
+//    }
+//
+//    if (size > 0)
+//    {
+//        memcpy(temp, in, size);     /* 0 ~ size-1 */
+//        temp[size] = 0x80;          /* size */
+//        memset(&temp[size+1], 0, AES_BLOCK_SIZE-size-1); /* size ~ AES_BLOCK_SIZE-1 */
+//
+//        AES_Decrypt(alg, temp, key, temp);
+//        xor(vector, temp, out);
+//    }
+
+    return ERR_OK;
+}
