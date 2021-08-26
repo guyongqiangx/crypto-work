38a9b6d3a78bccd62acf84a9ccc78bac53c95bbb
Yongqiang Gu <x_forever@126.com>
2021-08-26 19:24:50 +0800

    ZUC: 实现 EIA3 函数, 增加测试例子, 只有部分例子结果正确
    Signed-off-by: Yongqiang Gu <x_forever@126.com>

--- zuc/zuc.c
+++ zuc/zuc.c
@@ -320,12 +320,13 @@ static int EEA3(uint8_t *CK, uint32_t COUNT, uint32_t BEARER, uint32_t DIRECTION
     main = LENGTH / 32;
     rest = LENGTH % 32;
 
+    /* 逐字处理 32 bit 部分 */
     for (i=0; i<main; i++)
     {
         *OBS++ ^= *IBS ++;
     }
 
-    /* 消息长度不是 32 bit 整数倍的情况, 处理剩余部分 */
+    /* 处理不足 32 bit 的剩余部分 */
     if (rest)
     {
         /* 计算最后 32 bit */
@@ -336,7 +337,13 @@ static int EEA3(uint8_t *CK, uint32_t COUNT, uint32_t BEARER, uint32_t DIRECTION
 
     return ERR_OK;
 }
+#include <malloc.h>
 
+/* 取 offset 开始开始的 32 bit */
+static uint32_t GetWord(uint32_t S[2], uint32_t offset)
+{
+    return (S[0] << offset) | (S[1] >> (32-offset));
+}
 /*
  * 128-EIA3: EPS Integrity Algorithm 3, 完整性算法(Integrity)
  *        IK: 128-bit integrity key
@@ -347,16 +354,86 @@ static int EEA3(uint8_t *CK, uint32_t COUNT, uint32_t BEARER, uint32_t DIRECTION
  *         M: message
  *       MAC: message authentication code
  */
-static unsigned int EIA3(uint8_t *IK, uint32_t COUNT, uint32_t BEARER, uint32_t DIRECTION, uint32_t LENGTH, uint32_t *M, uint32_t *MAC)
+static int EIA3(uint8_t *IK, uint32_t COUNT, uint32_t BEARER, uint32_t DIRECTION, uint32_t LENGTH, uint32_t *M, uint32_t *MAC)
 {
+    ZUC_CTX ctx;
+    int i, j;
+    uint8_t iv[16];
+    uint32_t L, T, z;
+    uint32_t main, rest;
+    uint32_t *obs;
+
+    if ((NULL == IK) || (NULL == M) || (NULL == MAC) || (0 == LENGTH))
+    {
+        return ERR_INV_PARAM;
+    }
+
+    iv[ 0] = (COUNT >> 24) & 0xFF;
+    iv[ 1] = (COUNT >> 16) & 0xFF;
+    iv[ 2] = (COUNT >>  8) & 0xFF;
+    iv[ 3] = COUNT & 0xFF;
+    iv[ 4] = ((BEARER & 0x1F) << 3);
+    iv[ 5] = 0x00;
+    iv[ 6] = 0x00;
+    iv[ 7] = 0x00;
+
+    memcpy(&iv[8], &iv[0], 8);
+    iv[ 8] ^= DIRECTION << 7;
+    iv[14] ^= DIRECTION << 7;
+
+    initialize(&ctx, IK, iv);
+
+    L = (LENGTH + 31) / 32 + 2;
+    obs = (uint32_t *)malloc(L * sizeof(uint32_t));
+    work(&ctx, obs, L);
+
+    T = 0;
+
+    main = LENGTH / 32;
+    rest = LENGTH % 32;
+
+    /* 逐字处理 32 bit 部分 */
+    for (i=0; i<main; i++)
+    {
+        for (j=0; j<32; j++)
+        {
+            if (M[i] & (0x01 << (31-j)))
+            {
+                z = GetWord(&obs[i], j);
+                T ^= z;
+            }
+        }
+    }
+
+    /* 处理不足 32 bit 的剩余部分 */
+    if (rest)
+    {
+        for (j=0; j<rest; j++)
+        {
+            if (M[main] & (0x01 << (31-j)))
+            {
+                z = GetWord(&obs[main], j);
+                T ^= z;
+            }
+        }
+    }
+
+    /* T = T ^ Zlen */
+    z = GetWord(&obs[main], rest);
+    T ^= z;
+
+    T ^= obs[L-1];
+
+    free(obs);
+    obs = NULL;
+
+    *MAC = T;
+
     return ERR_OK;
 }
 
 #ifdef TEST
-
 #include <malloc.h>
-
-#define TEST_OUT_KEY_LEN 2
 
 static void TestingZUC(uint8_t *key, uint8_t *iv, uint32_t len)
 {
@@ -611,15 +688,164 @@ static void EEA3Tests(void)
 
 }
 
-void TestEIA3(void)
+// static void TestingEEA3(uint8_t *ck, uint32_t count, uint32_t bearer, uint32_t direction, uint32_t length, uint32_t *ibs)
+static void TestingEIA3(uint8_t *ik, uint32_t count, uint32_t bearer, uint32_t direction, uint32_t length, uint32_t *m)
 {
+    int i;
+    uint32_t size, mac;
+
+    EIA3(ik, count, bearer, direction, length, m, &mac);
+
+    size = (length + 31) / 32;
+    printf("Input Bit Stream:\n");
+    for (i=0; i<size; i++)
+    {
+        printf("0x%08x ", m[i]);
+        if (i%8 == 7)
+        {
+            printf("\n");
+        }
+    }
+    printf("\n");
+    printf("Output MAC: 0x%08x\n", mac);
+}
+
+static void EIA3Tests(void)
+{
+    {
+        /* Test Set 1 */
+        uint8_t ik[16] =
+        {
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+        };
+        uint32_t m[] = {
+            0x00000000,
+        };
+        uint32_t     count = 0x00;
+        uint32_t    bearer = 0x00;
+        uint32_t direction = 0x0;
+        uint32_t    length = 0x01; /* 1 bits */
+
+        printf("EIA3 Test Set 1:\n");
+        TestingEIA3(ik, count, bearer, direction, length, m);
+        printf("\n");
+    }
+
+    {
+        /* Test Set 2 */
+        uint8_t ik[16] =
+        {
+            0x47, 0x05, 0x41, 0x25, 0x56, 0x1e, 0xb2, 0xdd, 0xa9, 0x40, 0x59, 0xda, 0x05, 0x09, 0x78, 0x50
+        };
+        uint32_t m[] = {
+            0x00000000, 0x00000000, 0x00000000,
+        };
+        uint32_t     count = 0x561eb2dd;
+        uint32_t    bearer = 0x14;
+        uint32_t direction = 0x0;
+        uint32_t    length = 90; /* 90 bits */
+
+        printf("EIA3 Test Set 2:\n");
+        TestingEIA3(ik, count, bearer, direction, length, m);
+        printf("\n");
+    }
+
+    {
+        /* Test Set 3 */
+        uint8_t ik[16] =
+        {
+            0xc9, 0xe6, 0xce, 0xc4, 0x60, 0x7c, 0x72, 0xdb, 0x00, 0x0a, 0xef, 0xa8, 0x83, 0x85, 0xab, 0x0a
+        };
+        uint32_t m[] = {
+            0x983b41d4, 0x7d780c9e, 0x1ad11d7e, 0xb70391b1, 0xde0b35da, 0x2dc62f83, 0xe7b78d63, 0x06ca0ea0,
+            0x7e941b7b, 0xe91348f9, 0xfcb170e2, 0x217fecd9, 0x7f9f68ad, 0xb16e5d7d, 0x21e569d2, 0x80ed775c,
+            0xebde3f40, 0x93c53881, 0x00000000
+        };
+        uint32_t     count = 0xa94059da;
+        uint32_t    bearer = 0x0a;
+        uint32_t direction = 0x1;
+        uint32_t    length = 577; /* 577 bits */
+
+        printf("EIA3 Test Set 3:\n");
+        TestingEIA3(ik, count, bearer, direction, length, m);
+        printf("\n");
+    }
+
+    {
+        /* Test Set 4 */
+        uint8_t ik[16] =
+        {
+            0xc8, 0xa4, 0x82, 0x62, 0xd0, 0xc2, 0xe2, 0xba, 0xc4, 0xb9, 0x6e, 0xf7, 0x7e, 0x80, 0xca, 0x59
+        };
+        uint32_t m[] = {
+            0xb546430b, 0xf87b4f1e, 0xe834704c, 0xd6951c36, 0xe26f108c, 0xf731788f, 0x48dc34f1, 0x678c0522,
+            0x1c8fa7ff, 0x2f39f477, 0xe7e49ef6, 0x0a4ec2c3, 0xde24312a, 0x96aa26e1, 0xcfba5756, 0x3838b297,
+            0xf47e8510, 0xc779fd66, 0x54b14338, 0x6fa639d3, 0x1edbd6c0, 0x6e47d159, 0xd94362f2, 0x6aeeedee,
+            0x0e4f49d9, 0xbf841299, 0x5415bfad, 0x56ee82d1, 0xca7463ab, 0xf085b082, 0xb09904d6, 0xd990d43c,
+            0xf2e062f4, 0x0839d932, 0x48b1eb92, 0xcdfed530, 0x0bc14828, 0x0430b6d0, 0xcaa094b6, 0xec8911ab,
+            0x7dc36824, 0xb824dc0a, 0xf6682b09, 0x35fde7b4, 0x92a14dc2, 0xf4364803, 0x8da2cf79, 0x170d2d50,
+            0x133fd494, 0x16cb6e33, 0xbea90b8b, 0xf4559b03, 0x732a01ea, 0x290e6d07, 0x4f79bb83, 0xc10e5800,
+            0x15cc1a85, 0xb36b5501, 0x046e9c4b, 0xdcae5135, 0x690b8666, 0xbd54b7a7, 0x03ea7b6f, 0x220a5469,
+            0xa568027e,
+        };
+        uint32_t     count = 0x05097850;
+        uint32_t    bearer = 0x10;
+        uint32_t direction = 0x1;
+        uint32_t    length = 2079; /* 2079 bits */
+
+        printf("EIA3 Test Set 4:\n");
+        TestingEIA3(ik, count, bearer, direction, length, m);
+        printf("\n");
+    }
+
+    {
+        /* Test Set 5 */
+        uint8_t ik[16] =
+        {
+            0x6b, 0x8b, 0x08, 0xee, 0x79, 0xe0, 0xb5, 0x98, 0x2d, 0x6d, 0x12, 0x8e, 0xa9, 0xf2, 0x20, 0xcb
+        };
+        uint32_t m[] = {
+            0x5bad7247, 0x10ba1c56, 0xd5a315f8, 0xd40f6e09, 0x3780be8e, 0x8de07b69, 0x92432018, 0xe08ed96a,
+            0x5734af8b, 0xad8a575d, 0x3a1f162f, 0x85045cc7, 0x70925571, 0xd9f5b94e, 0x454a77c1, 0x6e72936b,
+            0xf016ae15, 0x7499f054, 0x3b5d52ca, 0xa6dbeab6, 0x97d2bb73, 0xe41b8075, 0xdce79b4b, 0x86044f66,
+            0x1d4485a5, 0x43dd7860, 0x6e0419e8, 0x059859d3, 0xcb2b67ce, 0x0977603f, 0x81ff839e, 0x33185954,
+            0x4cfbc8d0, 0x0fef1a4c, 0x8510fb54, 0x7d6b06c6, 0x11ef44f1, 0xbce107cf, 0xa45a06aa, 0xb360152b,
+            0x28dc1ebe, 0x6f7fe09b, 0x0516f9a5, 0xb02a1bd8, 0x4bb0181e, 0x2e89e19b, 0xd8125930, 0xd178682f,
+            0x3862dc51, 0xb636f04e, 0x720c47c3, 0xce51ad70, 0xd94b9b22, 0x55fbae90, 0x6549f499, 0xf8c6d399,
+            0x47ed5e5d, 0xf8e2def1, 0x13253e7b, 0x08d0a76b, 0x6bfc68c8, 0x12f375c7, 0x9b8fe5fd, 0x85976aa6,
+            0xd46b4a23, 0x39d8ae51, 0x47f680fb, 0xe70f978b, 0x38effd7b, 0x2f7866a2, 0x2554e193, 0xa94e98a6,
+            0x8b74bd25, 0xbb2b3f5f, 0xb0a5fd59, 0x887f9ab6, 0x8159b717, 0x8d5b7b67, 0x7cb546bf, 0x41eadca2,
+            0x16fc1085, 0x0128f8bd, 0xef5c8d89, 0xf96afa4f, 0xa8b54885, 0x565ed838, 0xa950fee5, 0xf1c3b0a4,
+            0xf6fb71e5, 0x4dfd169e, 0x82cecc72, 0x66c850e6, 0x7c5ef0ba, 0x960f5214, 0x060e71eb, 0x172a75fc,
+            0x1486835c, 0xbea65344, 0x65b055c9, 0x6a72e410, 0x52241823, 0x25d83041, 0x4b40214d, 0xaa8091d2,
+            0xe0fb010a, 0xe15c6de9, 0x0850973b, 0xdf1e423b, 0xe148a237, 0xb87a0c9f, 0x34d4b476, 0x05b803d7,
+            0x43a86a90, 0x399a4af3, 0x96d3a120, 0x0a62f3d9, 0x507962e8, 0xe5bee6d3, 0xda2bb3f7, 0x237664ac,
+            0x7a292823, 0x900bc635, 0x03b29e80, 0xd63f6067, 0xbf8e1716, 0xac25beba, 0x350deb62, 0xa99fe031,
+            0x85eb4f69, 0x937ecd38, 0x7941fda5, 0x44ba67db, 0x09117749, 0x38b01827, 0xbcc69c92, 0xb3f772a9,
+            0xd2859ef0, 0x03398b1f, 0x6bbad7b5, 0x74f7989a, 0x1d10b2df, 0x798e0dbf, 0x30d65874, 0x64d24878,
+            0xcd00c0ea, 0xee8a1a0c, 0xc753a279, 0x79e11b41, 0xdb1de3d5, 0x038afaf4, 0x9f5c682c, 0x3748d8a3,
+            0xa9ec54e6, 0xa371275f, 0x1683510f, 0x8e4f9093, 0x8f9ab6e1, 0x34c2cfdf, 0x4841cba8, 0x8e0cff2b,
+            0x0bcc8e6a, 0xdcb71109, 0xb5198fec, 0xf1bb7e5c, 0x531aca50, 0xa56a8a3b, 0x6de59862, 0xd41fa113,
+            0xd9cd9578, 0x08f08571, 0xd9a4bb79, 0x2af271f6, 0xcc6dbb8d, 0xc7ec36e3, 0x6be1ed30, 0x8164c31c,
+            0x7c0afc54, 0x1c000000,
+        };
+        uint32_t     count = 0x561eb2dd;
+        uint32_t    bearer = 0x1c;
+        uint32_t direction = 0x0;
+        uint32_t    length = 5670; /* 5670 bits */
+
+        printf("EIA3 Test Set 5:\n");
+        TestingEIA3(ik, count, bearer, direction, length, m);
+        printf("\n");
+    }
 
 }
 
 int main(int argc, char *argv[])
 {
-    ZUCTests();
+    //ZUCTests();
     //EEA3Tests();
+    EIA3Tests();
 
     return 0;
 }
