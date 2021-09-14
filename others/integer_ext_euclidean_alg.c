10a3ccd451cde7320a74198a9bead7e3c3314c59
Yongqiang Gu <x_forever@126.com>
2021-09-14 16:21:47 +0800

    EXT ECULIDIAN: 实现基于整数的 扩展欧几里得算法
    Signed-off-by: Yongqiang Gu <x_forever@126.com>

---
+++ others/integer_ext_euclidean_alg.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+
+/*
+ * 示例:
+ * gcd(3, 0) = 3
+ * gcd(0, 3) = gcd(3, 0) = 3
+ * gcd(50, 30) = gcd(30, 20) = gcd(20, 10) = gcd(10, 0) = 10
+ * gcd(30, 50) = gcd(50, 30) = ...
+ */
+int gcd(int a, int b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+    else
+    {
+        return gcd(b, a % b);
+    }
+}
+
+/* ax + by = 1 mod b */
+int ext_euclidian(int a, int b, int *ia, int *ib)
+{
+    int x, y, x0, y0, x1, y1;
+    int q, r;
+
+    x0 = 1; y0 = 0;
+    x1 = 0; y1 = 1;
+
+    q = a / b;
+    r = a % b;
+
+    while (r != 0)
+    {
+        /* 计算当前项 x/y */
+        x = x0 - q * x1;
+        y = y0 - q * y1;
+
+        /* 依次保存前两项到 x0/y0, x1/y1 */
+        x0 = x1; x1 = x;
+        y0 = y1; y1 = y;
+
+        a = b;
+        b = r;
+
+        q = a / b;
+        r = a % b;
+    }
+
+    *ia = x;
+    *ib = y;
+
+    return 0;
+}
+
+/* ax + by = 1 mod b */
+int multi_inverse(int a, int b)
+{
+    int ia, ib;
+
+    ext_euclidian(a, b, &ia, &ib);
+    if (ia < 0)
+    {
+        ia += b;
+    }
+
+    return ia;
+}
+
+int main(int argc, char *argv[])
+{
+    int x;
+    int ia, ib;
+
+    x = gcd(33, 18);
+    printf("gcd(33,18)=%d\n", x);
+
+    x = gcd(100, 29);
+    printf("gcd(100,29)=%d\n", x);
+
+    ext_euclidian(1759, 550, &ia, &ib);
+    printf("ext_euclidian(1759, 550) = (%d, %d)\n", ia, ib);
+    printf("%d x (%d) + %d x (%d) = %d\n", 1759,  ia, 550, ib, 1759 * ia + 550 * ib);
+
+    x = multi_inverse(1759, 550);
+    printf("multi_inverse(1759, 550) = %d\n", x);
+
+    x = multi_inverse(550, 1759);
+    printf("multi_inverse(550, 1759) = %d\n", x);
+
+    return 0;
+}
