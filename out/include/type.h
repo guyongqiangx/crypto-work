553be6521da2c1b50341751cbfe01827dccd802e
Yongqiang Gu <x_forever@126.com>
2021-10-22 15:32:24 +0800

    HASH: 更新 hash 库的 type.h 头文件
    Signed-off-by: Yongqiang Gu <x_forever@126.com>

--- out/include/type.h
+++ out/include/type.h
@@ -17,10 +17,15 @@
 typedef unsigned char      uint8_t;
 typedef unsigned short     uint16_t;
 typedef unsigned int       uint32_t;
+#ifndef uint64_t
 typedef unsigned long long uint64_t;
+#endif
+
+#ifndef uint128_t
 typedef struct {
     uint64_t high; /* high 64 bits */
     uint64_t low;  /*  low 64 bits */
 } uint128_t;
+#endif
 
 #endif
