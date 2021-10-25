26ec21dbec58621c659e6bcab6c1f5b0cd28ef95
Yongqiang Gu <x_forever@126.com>
2021-10-25 16:39:56 +0800

    MGF: 更新 mgf 相关的库和头文件
    Signed-off-by: Yongqiang Gu <x_forever@126.com>

--- out/include/mgf.h
+++ out/include/mgf.h
@@ -1,6 +1,13 @@
 #ifndef __ROCKY_MGF__H
 #define __ROCKY_MGF__H
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 
 int MGF1(const char *mgfSeed, unsigned int mgfSeedLen, HASH_ALG alg, unsigned int maskLen, char *mask);
 
+#ifdef __cplusplus
+}
+#endif
 #endif
