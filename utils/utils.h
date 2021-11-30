7dd6d9f4b625bc1d9e1854db37442e2ca7e3f03c
Yongqiang Gu <x_forever@126.com>
2021-11-30 18:42:07 +0800

    UTILS: 新增宏 DISABLE_DUMP_FUNCTIONS 用于关闭 dump 函数
    Signed-off-by: Yongqiang Gu <x_forever@126.com>

--- utils/utils.h
+++ utils/utils.h
@@ -8,7 +8,7 @@ extern "C"
 void dumphex(const void *data, int data_size, const char *tips, const char *indent, int line_size);
 void dump(const void *data, int size, const char *tips);
 
-#ifdef DEBUG
+#ifdef DISABLE_DUMP_FUNCTIONS
 #define dumphex(...)
 #define dump(...)
 #endif
