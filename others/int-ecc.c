0b54fe3ca45487ebb0370acf6667edeefc2cb051
Yongqiang Gu <x_forever@126.com>
2021-12-09 18:09:15 +0800

    Others: 添加《深入浅出密码学》和《密码编码学与网络安全》中的多个示例
    Signed-off-by: Yongqiang Gu <x_forever@126.com>

--- others/int-ecc.c
+++ others/int-ecc.c
@@ -135,21 +135,147 @@ int main(int argc, char *argv)
      * Elliptic Curve: y^2 = x^3 + 2x + 2 mod 17
      * Base Point (5, 1)
      */
+    printf("Elliptic Curve: y^2 = x^3 + 2x + 2 mod 17, Base Point (5, 1)\n");
     p = 17; a = 2; b = 2;
     x1 = 5; y1 = 1;
 
     i = 1;
-    printf("%2dP(%2d, %2d)\n", i, x1, y1);
+    printf("%4dP(%4d, %4d)\n", i, x1, y1);
 
     i++;
     ecc_points_double(p, a, x1, y1, &x2, &y2);
-    printf("%2dP(%2d, %2d)\n", i, x2, y2);
+    printf("%4dP(%4d, %4d)\n", i, x2, y2);
 
     while (x2 != x1)
     {
         i++;
         ecc_points_add(p, x1, y1, x2, y2, &x, &y);
-        printf("%2dP(%2d, %2d)\n", i, x, y);
+        printf("%4dP(%4d, %4d)\n", i, x, y);
+        x2 = x;
+        y2 = y;
+    }
+
+    /*
+     * 深入浅出密码学, p242, Q9.5
+     * Elliptic Curve: y^2 = x^3 + 3x + 2 mod 7
+     * Base Point (2, 4)
+     */
+    printf("Elliptic Curve: y^2 = x^3 + 3x + 2 mod 7, Base Point (2, 4)\n");
+    p = 7; a = 3; b = 2;
+    // x1 = 2; y1 = 4;
+    x1 = 0; y1 = 3;
+
+    i = 1;
+    printf("%4dP(%4d, %4d)\n", i, x1, y1);
+
+    i++;
+    ecc_points_double(p, a, x1, y1, &x2, &y2);
+    printf("%4dP(%4d, %4d)\n", i, x2, y2);
+
+    while (x2 != x1)
+    {
+        i++;
+        ecc_points_add(p, x1, y1, x2, y2, &x, &y);
+        printf("%4dP(%4d, %4d)\n", i, x, y);
+        x2 = x;
+        y2 = y;
+    }
+
+    /*
+     * 深入浅出密码学, p242, Q9.7
+     * Elliptic Curve: y^2 = x^3 + 4x + 20 mod 29
+     * Base Point (8, 10)
+     */
+    printf("Elliptic Curve: y^2 = x^3 + 4x + 20 mod 29, Base Point (8, 10)\n");
+    p = 29; a = 4; b = 20;
+    x1 = 8; y1 = 10;
+
+    i = 1;
+    printf("%4dP(%4d, %4d)\n", i, x1, y1);
+
+    i++;
+    ecc_points_double(p, a, x1, y1, &x2, &y2);
+    printf("%4dP(%4d, %4d)\n", i, x2, y2);
+
+    while (x2 != x1)
+    {
+        i++;
+        ecc_points_add(p, x1, y1, x2, y2, &x, &y);
+        printf("%4dP(%4d, %4d)\n", i, x, y);
+        x2 = x;
+        y2 = y;
+    }
+
+    /*
+     * 深入浅出密码学, p243, Q9.9
+     * Elliptic Curve: y^2 = x^3 + x + 6 mod 11
+     * Base Point (5, 9)
+     */
+    printf("Elliptic Curve: y^2 = x^3 + x + 6 mod 11, Base Point (5, 9)\n");
+    p = 11; a = 1; b = 6;
+    x1 = 5; y1 = 9;
+
+    i = 1;
+    printf("%4dP(%4d, %4d)\n", i, x1, y1);
+
+    i++;
+    ecc_points_double(p, a, x1, y1, &x2, &y2);
+    printf("%4dP(%4d, %4d)\n", i, x2, y2);
+
+    while (i++<6)
+    {
+        ecc_points_add(p, x1, y1, x2, y2, &x, &y);
+        printf("%4dP(%4d, %4d)\n", i, x, y);
+        x2 = x;
+        y2 = y;
+    }
+
+    /*
+     * 密码编码学与网络安全, 7th, section 10.4, p226
+     * Elliptic Curve: y^2 = x^3 + 9x + 17 mod 23
+     * Base Point (9, 17)
+     */
+    printf("Elliptic Curve: y^2 = x^3 + 9x + 17 mod 23, Base Point (16, 5)\n");
+    p = 23; a = 9; b = 17;
+    x1 = 16; y1 = 5;
+
+    i = 1;
+    printf("%4dP(%4d, %4d)\n", i, x1, y1);
+
+    i++;
+    ecc_points_double(p, a, x1, y1, &x2, &y2);
+    printf("%4dP(%4d, %4d)\n", i, x2, y2);
+
+    while (x2 != x1)
+    {
+        i++;
+        ecc_points_add(p, x1, y1, x2, y2, &x, &y);
+        printf("%4dP(%4d, %4d)\n", i, x, y);
+        x2 = x;
+        y2 = y;
+    }
+
+    /*
+     * 密码编码学与网络安全, 7th, section 10.4.1, p227
+     * Elliptic Curve: y^2 = x^3 - 4 mod 211
+     * Base Point (2, 2)
+     */
+    printf("Elliptic Curve: Elliptic Curve: y^2 = x^3 - 4 mod 211, Base Point (2, 2)\n");
+    p = 211; a = 0; b = -4;
+    x1 = 2; y1 = 2;
+
+    i = 1;
+    printf("%4dP(%4d, %4d)\n", i, x1, y1);
+
+    i++;
+    ecc_points_double(p, a, x1, y1, &x2, &y2);
+    printf("%4dP(%4d, %4d)\n", i, x2, y2);
+
+    while (x2 != x1)
+    {
+        i++;
+        ecc_points_add(p, x1, y1, x2, y2, &x, &y);
+        printf("%4dP(%4d, %4d)\n", i, x, y);
         x2 = x;
         y2 = y;
     }
